232ecbebd64a054808bf9fd1fad1dab37c058824
teller2016 <teller75@naver.com>
2023-02-15 22:18:59 +0900

    refactor: delete unnecesarry component

--- src/components/ContainerBox.vue
+++ src/components/ContainerBox.vue
@@ -3,31 +3,42 @@
     <!-- 옵션 영역 -->
     <div class="container__box">
       <div class="sb__options">
-        <OptionBox headerTitle="날짜설정">
+        <div class="sb__option">
+          <header class="option__header">날짜설정</header>
 
-          <SelectElement selectTitle="년도" selectName="year">
-            <option value="22">22</option>
-            <option value="23" selected>23</option>
-            <option value="24">24</option>
-            <option value="25">25</option>
-            <option value="26">26</option>
-          </SelectElement>
+          <div class="option__content">
+              <SelectElement selectTitle="년도" selectName="year">
+                <option value="22">22</option>
+                <option value="23" selected>23</option>
+                <option value="24">24</option>
+                <option value="25">25</option>
+                <option value="26">26</option>
+              </SelectElement>
+        
+              <SelectElement selectTitle="출근시간" selectName="start-time">
+                <option value="18">9</option>
+                <option value="18.5" selected="">9:30</option>
+                <option value="19">10:00</option>
+              </SelectElement>
+          </div>
+      </div>
 
-          <SelectElement selectTitle="출근시간" selectName="start-time">
-            <option value="18">9</option>
-            <option value="18.5" selected="">9:30</option>
-            <option value="19">10:00</option>
-          </SelectElement>
+        <div class="sb__option">
+          <header class="option__header">일지등록</header>
 
-        </OptionBox>
+          <div class="option__content">
+            <p class="sb__options__guide">해당 월의 일지 excel파일을 불러와 주세요.</p>
+            <DragDrop/>
+          </div>
+      </div>
 
-        <OptionBox headerTitle="일지 등록">
-          <p class="sb__options__guide">해당 월의 일지 excel파일을 불러와 주세요.</p>
+      <div class="sb__option">
+          <header class="option__header">석식등록</header>
 
-          <DragDrop/>
-          
-        </OptionBox>
-        <OptionBox headerTitle="석식 등록"></OptionBox>
+          <div class="option__content">
+           
+          </div>
+      </div>
       </div>
     </div>
 
@@ -40,11 +51,9 @@
 
 <script>
 import SelectElement from './elements/SelectElement.vue';
-import OptionBox from './OptionBox.vue';
 import DragDrop from './DragDrop.vue';
 export default {
   components: {
-    OptionBox,
     SelectElement,
     DragDrop
   }
@@ -72,5 +81,25 @@ export default {
       font-size: 13px;
     }
   }
+
+  &__option {
+        width: 100%;
+        padding: 16px;
+
+        &:not(:first-of-type) {
+            border-left: 1px solid black;
+        }
+        .option {
+          &__header {
+              margin-bottom: 12px;
+              font-weight: 700;
+              font-size: 24px;
+              text-align: left;
+          }
+
+          &__content {
+          }
+        }
+    }
 }
 </style>
